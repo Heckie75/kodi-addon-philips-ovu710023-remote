40015a090befb600389176140403a0a925055588
Heckie75 <github@heckie.de>
2020-11-27 20:37:01 +0100

    Major refactoring

--- script.service.philips-ovu710023-remote/remote.py
+++ script.service.philips-ovu710023-remote/remote.py
@@ -1,15 +1,11 @@
+import json
+import os
 import re
 import threading
 import time
 import subprocess
 import xbmc
-
-__PLUGIN_ID__ = "script.service.philips-ovu710023-remote"
-
-PHILIPS_OVU710023_KEYBOARD = "PHILIPS OVU710023 Keyboard"
-PHILIPS_OVU710023_SYSTEM_CONTROL = "PHILIPS OVU710023 System Control"
-PHILIPS_OVU710023_CONSUMER_CONTROL = "PHILIPS OVU710023 Consumer Control"
-PHILIPS_OVU710023_MOUSE = "PHILIPS OVU710023 Mouse"
+import xbmcaddon
 
 KEY_STATE_UP = 0
 KEY_STATE_DOWN = 1
@@ -24,101 +20,26 @@ MODIFIER_KEYS = {
     "KEY_RIGHTALT": 100
 }
 
-INPUTS = {
-    PHILIPS_OVU710023_KEYBOARD: {
-        "KEY_ESC": [1],
-        "KEY_ENTER": [28],
-        "KEY_UP": [103],
-        "KEY_LEFT": [105],
-        "KEY_RIGHT": [106],
-        "KEY_DOWN": [108],
-        "PHILIPS_OVU710023_KEY_0": [56, 75, 72],
-        "PHILIPS_OVU710023_KEY_1": [56, 75, 73],
-        "PHILIPS_OVU710023_KEY_2": [56, 76, 82],
-        "PHILIPS_OVU710023_KEY_3": [56, 76, 79],
-        "PHILIPS_OVU710023_KEY_4": [56, 76, 80],
-        "PHILIPS_OVU710023_KEY_5": [56, 76, 81],
-        "PHILIPS_OVU710023_KEY_6": [56, 76, 75],
-        "PHILIPS_OVU710023_KEY_7": [56, 76, 76],
-        "PHILIPS_OVU710023_KEY_8": [56, 76, 77],
-        "PHILIPS_OVU710023_KEY_9": [56, 76, 71],
-        "PHILIPS_OVU710023_KEY_HASH": [56, 81, 76],
-        "PHILIPS_OVU710023_KEY_ASTERISK": [42, 9]
-    },
-    PHILIPS_OVU710023_SYSTEM_CONTROL: {
-        "KEY_SLEEP": [142],
-        "KEY_WAKEUP": [143]
-    },
-    PHILIPS_OVU710023_CONSUMER_CONTROL: {
-        "KEY_MUTE": [113],
-        "KEY_VOLUMEDOWN": [114],
-        "KEY_VOLUMEUP": [115],
-        "KEY_PAUSE": [119],
-        "KEY_PROPS": [130],
-        "KEY_BACK": [158],
-        "KEY_NEXTSONG": [163],
-        "KEY_PREVIOUSSONG": [165],
-        "KEY_STOPCD": [166],
-        "KEY_RECORD": [167],
-        "KEY_REWIND": [168],
-        "KEY_PLAY": [207],
-        "KEY_PROGRAM": [362],
-        "KEY_FASTFORWARD": [208],
-        "KEY_CHANNELUP": [402],
-        "KEY_CHANNELDOWN": [403]
-    }
-}
-
-ACTIONS = {
-    "KEY_ESC": "Action(OSD)",
-    "KEY_ENTER": "Action(Select)",
-    "KEY_UP": "Action(Up)",
-    "KEY_LEFT": "Action(Left)",
-    "KEY_RIGHT": "Action(Right)",
-    "KEY_DOWN": "Action(Down)",
-    "PHILIPS_OVU710023_KEY_0": "Action(Number0)",
-    "PHILIPS_OVU710023_KEY_1": "Action(Number1)",
-    "PHILIPS_OVU710023_KEY_2": "Action(Number2)",
-    "PHILIPS_OVU710023_KEY_3": "Action(Number3)",
-    "PHILIPS_OVU710023_KEY_4": "Action(Number4)",
-    "PHILIPS_OVU710023_KEY_5": "Action(Number5)",
-    "PHILIPS_OVU710023_KEY_6": "Action(Number6)",
-    "PHILIPS_OVU710023_KEY_7": "Action(Number7)",
-    "PHILIPS_OVU710023_KEY_8": "Action(Number8)",
-    "PHILIPS_OVU710023_KEY_9": "Action(Number9)",
-    "PHILIPS_OVU710023_KEY_HASH": "Action(ContextMenu)",
-    "PHILIPS_OVU710023_KEY_ASTERISK": "Action(Menu)",
-    "KEY_SLEEP": "ActivateWindow(ShutdownMenu)",
-    "KEY_WAKEUP": "Action(togglefullscreen)",
-    "KEY_MUTE": "Action(Mute)",
-    "KEY_VOLUMEDOWN": "Action(VolumeDown)",
-    "KEY_VOLUMEUP": "Action(VolumeUp)",
-    "KEY_BACK": "Action(Back)",
-    "KEY_PAUSE": "Action(Pause)",
-    "KEY_PROPS": "Action(Info)",
-    "KEY_NEXTSONG": "Action(SkipNext)",
-    "KEY_PREVIOUSSONG": "Action(SkipPrevious)",
-    "KEY_STOPCD": "Action(Stop)",
-    "KEY_RECORD": "Action(Record)",
-    "KEY_REWIND": "Action(Rewind)",
-    "KEY_PLAY": "Action(Play)",
-    "KEY_PROGRAM": "Action(Playlist)",
-    "KEY_FASTFORWARD": "Action(FastForward)",
-    "KEY_CHANNELUP": "Action(ChannelUp)",
-    "KEY_CHANNELDOWN": "Action(ChannelDown)"
-}
-
+__PLUGIN_ID__ = "script.service.philips-ovu710023-remote"
+settings = xbmcaddon.Addon(id=__PLUGIN_ID__)
+addon_dir = xbmc.translatePath(settings.getAddonInfo('path'))
 
 class Listener(xbmc.Monitor):
 
-    _listeners = None
+    _config = None
+    _listeners = []
     _last_action_ts = 0
+    _last_scan_inputs = {}
 
     def __init__(self):
 
         xbmc.Monitor.__init__(self)
 
-        self._listeners = []
+        self._load_config()
+
+    def _load_config(self):
+        data = open(os.path.join(addon_dir, "resources", "remote.json"), "r").read()
+        self._config = json.loads(data)
 
     def _evtest_find_devices(self):
 
@@ -136,7 +57,7 @@ class Listener(xbmc.Monitor):
 
         return devices
 
-    def start(self):
+    def startup(self):
 
         def _has_listener(_i):
             for _l in self._listeners:
@@ -145,7 +66,7 @@ class Listener(xbmc.Monitor):
             return False
 
         _input_devices = self._evtest_find_devices()
-        for _input in INPUTS:
+        for _input in self._config:
             if _input in _input_devices and not _has_listener(_input):
                 self._start(_input, _input_devices[_input])
                 time.sleep(0.2)
@@ -222,33 +143,32 @@ class Listener(xbmc.Monitor):
 
     def _apply_sequence(self, sequence):
 
-        def _match_event(sequence):
+        def _match_sequence(sequence):
 
             if sequence == []:
-                return None
+                return None, None
 
-            for known_inputs in INPUTS:
-                for known_event in INPUTS[known_inputs]:
-                    if INPUTS[known_inputs][known_event] == sequence:
-                        return known_event
+            for known_inputs in self._config:
+                for definition in self._config[known_inputs]:
+                    if self._config[known_inputs][definition]["seq"] == sequence:
+                        return definition, self._config[known_inputs][definition]["action"]
 
-            return None
+            return None, None
 
-        found_event = _match_event(sequence)
-        if found_event:
-            xbmc.log("[Philips remote] event found: %s" %
-                     found_event, xbmc.LOGDEBUG)
-            if self._is_display_on():
-                xbmc.executebuiltin(ACTIONS[found_event])
+        definition, action = _match_sequence(sequence)
+        if definition:
+            xbmc.log("[Philips remote] found action: %s --> %s" % (definition, action), xbmc.LOGDEBUG)
+            if not self._turn_display_on():
+                xbmc.executebuiltin(action)
             return True
 
         return False
 
-    def _is_display_on(self):
+    def _turn_display_on(self):
 
         current_time = time.time()
         if self._last_action_ts + 299 > current_time:
-            return True
+            return False
 
         ps = subprocess.Popen(
             ["xset", "-q"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -257,11 +177,19 @@ class Listener(xbmc.Monitor):
         if not re.search("Monitor is On", stdout):
             xbmc.log("[Philips remote] turn monitor on", xbmc.LOGNOTICE)
             subprocess.call(["xset", "dpms", "force", "on"])
-            return False
-        else:
             return True
+        else:
+            return False
 
-    def clean(self):
+    def scan_inputs(self):
+
+        _current = set(filter(lambda e: e.startswith("event"), os.listdir("/dev/input")))
+        _new = _current - self._last_scan_inputs
+        _old = self._last_scan_inputs - _current
+        self._last_scan_inputs = _current
+        return _old, _new
+        
+    def shutdown(self):
 
         for l in self._listeners:
             l["subprocess"].kill()
@@ -270,12 +198,12 @@ class Listener(xbmc.Monitor):
 if __name__ == "__main__":
     xbmc.log("[Philips remote] Service started", xbmc.LOGNOTICE)
     listener = Listener()
-    listener.start()
+    listener.startup()
 
     while not listener.abortRequested():
         if listener.waitForAbort(10):
             pass
 
     xbmc.log("[Philips remote] stopping service", xbmc.LOGNOTICE)
-    listener.clean()
+    listener.shutdown()
     xbmc.log("[Philips remote] Service stopped.", xbmc.LOGNOTICE)
